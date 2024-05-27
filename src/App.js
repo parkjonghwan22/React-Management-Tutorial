cac90246846c1e0b888d9d4a2691427f491644f0
parkjonghwan22 <gch02933@naver.com>
2024-05-27 20:00:10 +0900

    feat: Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import { Customer } from './Components/Customer';
+import Paper from '@mui/material/Paper';
+import Table  from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead'
+import TableBody from '@mui/material/TableBody'
+import TableRow from '@mui/material/TableRow'
+import TableCell from '@mui/material/TableCell'
+import { styled } from '@mui/system';
+import React from 'react'
+
+const Root = styled(Paper)(({ theme }) => ({
+  width: '100%',
+  marginTop: theme.spacing(3),
+  overflowX: 'auto',
+}));
+// Paper, Table 컴포넌트를 styled 함수가 스타일링한다
+// theme 은 Material-UI의 테마 객체로, 스타일링에 사용되는 다양한 값들을 포함한다
+
+const CustomTable = styled(Table)({
+  minWidth: 1080,
+});
 
 const customers = [
   {
     'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
     'name': '홍길동',
     'birthday': '920313',
     'gender': '여자',
@@ -13,7 +32,6 @@ const customers = [
   },
   {
     'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
     'name': '박종환',
     'birthday': '950313',
     'gender': '남자',
@@ -21,7 +39,6 @@ const customers = [
   },
   {
     'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
     'name': '임준범',
     'birthday': '970313',
     'gender': '남자',
@@ -31,22 +48,34 @@ const customers = [
 
 function App() {
   return (
-    <div>
-      {
-        customers.map(c => (
-          <Customer 
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-          />
+    <Root>
+      <CustomTable>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+            customers.map(c => (
+              <Customer 
+                key={c.id}
+                id={c.id}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
 
-        ))
-      }
-    </div>
+            ))
+          }
+        </TableBody>
+      </CustomTable>
+    </Root>
   );
 }
 
